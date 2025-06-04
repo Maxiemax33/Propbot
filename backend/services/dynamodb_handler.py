f761646f27b817a7b04fd566d340c33217cb9fd3
Maxiemax33 <shaniyajoseph29@gmail.com>
2025-06-05 03:34:18 +0530

    Update dynamodb_handler.py

--- backend/services/dynamodb_handler.py
+++ backend/services/dynamodb_handler.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 # Use your named profile
 dynamodb = boto3.resource("dynamodb", region_name="ap-south-1")
-dynamodb = session.resource("dynamodb")
 table = dynamodb.Table("propbot_listings")
 
 def save_listing_metadata(s3_url: str, filename: str):
