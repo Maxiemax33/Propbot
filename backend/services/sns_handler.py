3efb18c044e3f0b740be3f8b74775ba9ce5b358d
Maxiemax33 <shaniyajoseph29@gmail.com>
2025-06-05 03:37:35 +0530

    Update sns_handler.py

--- backend/services/sns_handler.py
+++ backend/services/sns_handler.py
@@ -2,8 +2,8 @@ import boto3
 
 TOPIC_ARN = "arn:aws:sns:ap-south-1:072244248629:propbot-risk-alerts"
 
-dynamodb = boto3.resource("dynamodb", region_name="ap-south-1")
-sns = session.client("sns", region_name="ap-south-1")
+
+sns = boto3.client("sns", region_name="ap-south-1")
 
 def send_risk_alert(listing_id, summary):
     message = f"⚠️ Risk Alert for Listing ID: {listing_id}\n\n{summary}"
